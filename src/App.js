fded2d824913ba8c46f7fea61a224d66ed6f4b41
trecho26 <hectorlara260698@gmail.com>
2020-06-08 15:38:48 -0600

    bolierplate base para trabajar con react

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from "react";
+import HelloWorld from "./Components/HelloWorld";
+
+const App = () => {
+  return (
+    <Fragment>
+      <HelloWorld />
+    </Fragment>
+  );
+};
+
+export default App;
---
+++ src/Components/HelloWorld.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "../assets/styles/App.scss";
+
+const HelloWorld = () => {
+  return (
+    <div>
+      <h1>Hola mundo</h1>
+    </div>
+  );
+};
+
+export default HelloWorld;
